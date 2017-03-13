e54077ff5cd266c02b2051f61bc18791b1eef4d2
Grady Kuhnline <github@heygrady.net>
2017-03-13 12:08:19 -0700

    switching to lodash.get

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,8 +1,10 @@
-export const selectValueByKey = item => key => item !== undefined ? item[key] : undefined
-export const selectData = item => selectValueByKey(item)('data')
-export const selectMeta = item => selectValueByKey(item)('meta')
-export const selectId = item => selectValueByKey(item)('id')
-export const selectType = item => selectValueByKey(item)('type')
+import get from 'lodash.get'
+
+export const selectValueByKey = item => key => get(item, key)
+export const selectData = item => get(item, 'data')
+export const selectMeta = item => get(item, 'meta')
+export const selectId = item => get(item, 'id')
+export const selectType = item => get(item, 'type')
 
 export * from './item'
 export * from './collection'
